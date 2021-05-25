4d41b48804271cb4bc3d1c7de02d7103182ab825
Manuel Ríos <manur.0612@gmail.com>
2021-05-25 17:39:52 -0500

    Carga inicial

--- appBusca/Startup.cs
+++ appBusca/Startup.cs
@@ -13,6 +13,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Microsoft.EntityFrameworkCore;
+using appBusca.Models;
 namespace appBusca
 {
     public class Startup
@@ -27,14 +29,8 @@ namespace appBusca
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlite(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+            services.AddDbContext<BuscaDbContext>(dco => dco.UseSqlServer("Server=.;Database=Busca;Integrated Security=true;"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
